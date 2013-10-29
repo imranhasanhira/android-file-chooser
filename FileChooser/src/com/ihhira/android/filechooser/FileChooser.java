df95912c4669974578065a138101f7563c8d4b11
Md Imran Hasan Hira <imranhasanhira@gmail.com>
2013-10-30 02:16:54 +0600

    function signature added

--- FileChooser/src/com/ihhira/android/filechooser/FileChooser.java
+++ FileChooser/src/com/ihhira/android/filechooser/FileChooser.java
@@ -27,11 +27,19 @@ import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * 
+ * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+ */
 public class FileChooser implements OnClickListener {
 	enum DialogType {
 		SELECT_FILE, SELECT_DIRECTORY, SAVE_AS
 	}
 
+	/**
+	 * 
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	interface FileSelectionCallback {
 		public void onSelect(File file);
 	}
@@ -45,6 +53,9 @@ public class FileChooser implements OnClickListener {
 	private File currentFile;
 	private ArrayList<String> fileList;
 
+	/**
+	 * 
+	 */
 	private FilenameFilter fileFilter = new FilenameFilter() {
 
 		@Override
@@ -64,6 +75,13 @@ public class FileChooser implements OnClickListener {
 	private Drawable fileDrawable;
 	private Drawable upFolderDrawable;
 
+	/**
+	 * @param context
+	 * @param title
+	 * @param dialogType
+	 * @param startingFile
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	public FileChooser(Context context, String title, DialogType dialogType,
 			File startingFile) {
 		this.dialogType = dialogType;
@@ -86,11 +104,19 @@ public class FileChooser implements OnClickListener {
 
 	}
 
+	/**
+	 * @param callback
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	public void show(FileSelectionCallback callback) {
 		this.callback = callback;
 		loadFilelist();
 	}
 
+	/**
+	 * 
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	private void loadFilelist() {
 		if (currentDialog != null) {
 			currentDialog.dismiss();
@@ -108,6 +134,11 @@ public class FileChooser implements OnClickListener {
 		currentDialog = builder.show();
 	}
 
+	/**
+	 * @param context
+	 * @param builder
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	private void addAdapter(Context context, Builder builder) {
 		fileList = new ArrayList<String>();
 
@@ -161,6 +192,11 @@ public class FileChooser implements OnClickListener {
 		builder.setAdapter(adapter, this);
 	}
 
+	/**
+	 * @param context
+	 * @param builder
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	private void addPositiveButton(final Context context, Builder builder) {
 		String positiveButtonText = "Ok";
 
@@ -181,6 +217,10 @@ public class FileChooser implements OnClickListener {
 				positiveButtonClickListener);
 	}
 
+	/**
+	 * @param filename
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	protected void completeSelection(String filename) {
 		currentDialog.dismiss();
 		if (callback != null) {
@@ -189,6 +229,10 @@ public class FileChooser implements OnClickListener {
 		}
 	}
 
+	/**
+	 * @param filename
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	protected void loadFilenameDialog(String filename) {
 		if (currentDialog != null) {
 			currentDialog.dismiss();
@@ -230,6 +274,10 @@ public class FileChooser implements OnClickListener {
 		currentDialog = builder.show();
 	}
 
+	/**
+	 * @param newFileName
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	protected void loadDeleteExistingFileDialog(final String newFileName) {
 		Builder builder = new Builder(context);
 		builder.setTitle("File already exist. Delete existing ?");
@@ -267,6 +315,11 @@ public class FileChooser implements OnClickListener {
 
 	}
 
+	/* 
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 *(non-Javadoc)
+	 * @see android.content.DialogInterface.OnClickListener#onClick(android.content.DialogInterface, int)
+	 */
 	@Override
 	public void onClick(DialogInterface dialog, int which) {
 		if (fileList.get(which).equals(PARENT_FILE_NAME)) {
@@ -287,6 +340,10 @@ public class FileChooser implements OnClickListener {
 		}
 	}
 
+	/**
+	 * 
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 */
 	class ImageLib {
 		public static final String FOLDER = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABGdBTUEAANbY1E9YMgAAAAlwSFlzAAAOwwAADsMBx2+oZAAAABp0RVh0U29mdHdhcmUAUGFpbnQuTkVUIHYzLjUuMTAw9HKhAAAPpUlEQVR4Xu2aSWxW1xXHXSmRUCtlkUWnXZVIkVqpVZVI7Sa7tou2q2yatGqEwjyFDJAwBEiYh5rBGJvRDoPNYIwBY2MGm8FgY4yZTOKkSaMq3SVSpWwqpACv53fP/d93/RV1XVJ/0tG58z3//zn3vvfd+6qqxn5jDIwxMMbAGANjDIwxMMbAGAOjGejuO/qtuq6bi7eevvX37WdufynZ1T38ZUP38Bc7zt7+wtJBGnqGv2jsuROkwYSyxnNl2so+2Hb61h8eKY5XHRn445au28XW08NFvcm2M8PFjrN3kq4/dTvkkZ3dHxQ783R3Vm51tLFx7q9uG3jukSGh+vi1ujoDiUDC9jN3iu0GZIvlSUMIwCkTOTsrgG+zdpR5mzvFxhNDbz4yBPzl+FBvbdetYt3xoaL+FBHgQEvxfCDHABIligjyioy8z8aO62sfCQKWt/Q9u7Hj5oMtRgAeZykoGkgT/gBWdFBWe/JWarPuGKSV/ahHjNR/rG7t++7/NAk1HTd+a4Z+vuHEjWJT581iU4eJ6RrSJkZMQZ005aqjbUjHPhs7sjEoM7Gyf27quDGyqeP6SE3njRGLCtcnro9sMEGvbx8K6erjrte3e9m6Y9eSrDk6OLLqyNWRVW1XR1a0DowsPzyQ9JKD/SNLW66MvHuof2RB86WRxQf6RhYf7O+ft6/3v2/E8/d0f2/N0Wv/woOrj14rVhwZLNYcHSpWtQ2aXAuy2mRFq+dXtF4NbcivNI14+WCx7PBAyFOf65CO7Zdbf/VT34e1XXr4qo3n8l7LQPHuoYFi8cErxTv7+4t3DvQXC5r7irf2Xgp63r7LxdtRyJOe33Q51M1v6vt6av3Jpx8agV1XB59YvP/Cy0zChJokDGoDzLUJ5uzpLd7Y3Vu8/v7F4jWTVxsuBHmt0YX07KhTOiufFeul87a0p1ya9Mxd54tZJugZO88V03ecK6aZTN7WU0ysP1tM2tpdTNx6NqQn1J0txm85U4yvPR3KJ5tMsXb0oT9zYfvshp5Xm8+cfyKRUN3aM27vhZGmlv5PHrRe+bRou/q3om3g0+KoaeTYoAvp44OfWbqU49c+S23UvlIz3hEbD2H8w/2fBG3zBTnU99fi4GUT02ZHsf/yx8X+Sx8Xzb0mpptM77v4UdFksvfCR8Uea7P7/Gh5//yHYdNtPPehP5JNGnpclHdtm7Np268ebO682bR478lxVdtO3557+MonyUjAH4vgEwmBlJIY6kVM0ORjGeDoF4gEfCSVckhgLkhAjyLASDhgAngR0NT7UQDPUwUt8HuMANLvRyIgAGkwAhy4awALOHpXJER67dHBuVX29taEcXhCRspjeSRQ9rCoqPQ4Y1SOk8ADPHoe8Hg9SIwAvE46eD96HuB7owDagRtgA4smGkjL44DfZWDZpOV98pL04mZlG04MNVXZa2wt4PEIhmKACEhEZGFMGcS4jl6O+TzU8bzICF6P46OZT4LXXT5OEeCh794HJHp39Hi5BKLXY6gr/AGaAyedgyYqaEPZ+vZrtVXbzwwv0XpUeMrwoLP1mwOUV8O6zsB5XydTYR5CPXoejwMe0HiZcA9phb5pAFMXPG/A0Qp5CAGsBI+TJgIU2vkrO2mWULkXsA9Ymb3Y2WN1SVX9qZsz5Q0MzUN0FLi4eeXrWCSEfsGzvq5FZL7RaQ4IkNdz0Ap7PC/vE94Ke4+Acq0T9nhS4MMaj54FsN5G0RBTvp06GeTXtF2dWVXTef1FrUUBEIhRkRHDWOEs77IzV25skMhY7u241uM6DxtcDHE0wHPQjIfXfe07aO36bHp4nDJA4d2wtjPvK+Q9CtzTigjeTrfa67vIWNl65cWq6uODv9EmhGdEhnZoDA6PKhNPO7g8UkpPa32PfrwBFE/l4S4SAJcTEIAT9vFxp81OIU9e652wJ5y1w+sPWgj7+N9F0cCrO/9bvI2TsvRQ32+q1rb1P9dsYRceQfExlK9NrdnkybiG86jJwzvt6iIzjhnWdVzn+2w+7fKEuHZ6hbxCPd/hPdRL0WYnD6c/YtmfNv1TFfD8Dx1li5ovPFe17GDvU2wyevbmz2FIwTshQrJnNEQ5Yf7Ykme1tj3voV6GfPlyw3zytNZ4esbHRx3gA+hswwO0NjsRoB1eXoeQErCHu/8jzaLC0vyRm7en+6mq6gNtT8Ksh6HWo7+BJQBxt9buTLl2cb2xhfUc1zTtwttbfJTJ28prV1eYa40DOG10ydu+1rWTl4cwHsaAFWB0+Ica/6aPPsgp/77Trtb+na7ae/DJqr0tWx+zTeS+jMbY3bbJ5MZrSYgkRYny7k0DbYABN6o8kqEwr/R4ucH5yw0eBmzydHx70+GKCCjXuXtWf811gpWfZEFE+AufkWP/bO/XNax7LPwnsHeBr3LD9OaFh1SusAWc6kVa/rxO6biJ5ePqWZ4/x8vneQxvHlHhJKl8dOk0SgcwOmTRSZWD9TOH/OyCcv7JihwOeEhDhP3t/ir9IbIBP8/DUOsx//NBWflC8mEgRi8pYbPKHlf5HxaltXvLwzlwvaSkx1p8m8NY37hc8nAP3ozAdViDp3Uwo0MbdF0g5taoQ5vq9qHPEwFbT90aTgbFdaj1KHAA0Hs4ewYvJl5XvqCIBPXNPe1vaYS2P8ODZK+tkKDjNT26tJbzsMaLeFsnTjqxAiBEbLaTKUWB0pDCYQ59vexWYYctw4mALV03+2SUHjcynskxvDJstTvr0aQdm3FIpxeU+IamnVgvMOGwNL6kANg3Ml/LemZrRw9ejEdsgAhHcHg0CqBUHoBanpMpNKdS1IVTqnhixYmWnRr1lWcCx4e6/nOXdS/xooHIc5X/sCrfwfUmlntXj6rgWb2gmE6bU9y9tYZDKEePSgeQFQJIyvnnB1Adx1GmcqXRNZ1OBOPY/4CuREBNx/XDhJteFB52ulv5B0OPJoVr2Kjk0XiEjlfVL4RxXLfazJTHuxgmzzoRDgJj3fjyvLG6/XoAjbfx5vp2k3BO6WeQGy0tMigLbTOhz5q2wcOJADt0fJ+NIg8/bTjl46R8jur5ChCBUAgrfNMzOXsEeRj7GtRJsrwokApf5XUgC5BwUGtAzF47Yb4eD2cdIKCpl0AKachK5TZGIMpk5ZGrjdkSuFbLxOkxEtdYfhSuNAbTTgYKaN6WesJNIDW21mp+aqwQjSfGKZwV1u5ZnUTnAJ0EJERAFA5V6YMAnnr0OtNr7bBXEbOqdaA2EWBHzGsESAbJMwDJQxFjBEQbT9hwspAlz0RhLOtfbZPnIUxaANEK4dx7CnO0PD4a0FAYVyRIc5qdlwFa7UI6krLs8JU1iQA7X18cDIkhRohox9Rak9fSWor3BCEkY3vdG+Qe9juEPHzdo/JMAGhtKoGQ51YqaDNc9YCwo/tQRhrPyru05xif8lAX21b2IW93BosTActa+uaG0IiGwDgDwRyN0yYTN5zqGG5pHcZwC6EXNyQ04ORBrUWMFRgBw3AZnIyNZYDlLiJoE7TuJ7yt32Hk5aQpd/F+akOefov2X5qTCHjvUN+MnOG0brKQgRSBYmC1V0gJjMItadZfNFReK41z4FyicOHB+uWyRAAB7hcyfsGiyxnV6zKFPGnaq09+URP6xQsazfFO86UZiYBFB/omrA03QD6hDKZMHlFYSTvj3Bw522XIOeulF0oPjDI8AlppY+i2KNw4Ua7bJMtDSOXt01Iji7ZLDl2xem8j4WJH7WkHsbqJok4ELGy+/EoiYEFT70vOpk/uLJZXXckjBkxMAzD1CVdn9PUytQnXZAKaX5NFozEoAMxAkEa4mcqvxHQ1JlDK0592CGnK6ZvXQwL9qA9pazN/X+9LiYCFTRdeEDsYDKtiMb/3k3HSAM1ZlReCh2I4Mxb3ePRh8iWWlkG6hpPRlC+yuz76C6ja4m0MJy/JSVIfytSHMuZGNB71jPX2nvMvJALm7D7/ezpxiYiB7zJZnGhpi7NbesCNyC8qRxmU+jnTjMN470XDmDzk4yUnaYRLTgxl/iShzknJyymjT2hv2m12YLo0ZT5h0T1n6mPt32js+V0iYHbDuV/TWGyhNWCY2AZeaJekmizoaByDyqsyQPWMg/EL9/eNGpuyfC61wwHc+C40oU0uEER+7p6LoS9j+th+Q7zogM81b98lJyHOoTE0h+aetbPnV4kAyzyvQcqOPqAEw0hTD+DKOvVDy1j6aOJwlR2NxfiUtzJuoFVmV9gZONIO0ts4mWqrMm6vQzq18bkoF0GyS2NM3db9fCLg1Z1nfgn7mijcpZugK8UNLI3GGO7gg1Hxrl7gKJNxGg8tkAsiGNrk7ah/yzyJpj3jhX6RKMor7VL/h9occQQbok0ztp/6RSJgxvauZ/VRAQMpHQyIQlmoM4Es0jJIdeTz/qQBwncFaq++lX34/kBE+LcIMR/nHUVStFFz6aMIJ83t0zx5P+Gi3fRtXc8mAqbWd/6MSSuFry5o7OW9Ka0yBnwriLchj6afPqp4Y/fFVEa52tCOjyyUD2PF+fK0xkarP2NDEHnIDf1i39cayzErbREBlE/a0vHTRMCU+s6fvGlfT1Dxpg04J3qAwV18IhFUWc6XFwB1g5yMvA/jza0YA0DMlZMe5o7A9EVKymfjBjttTs2hOWU/dpQ2ODn5eMwzua7zx4mACbUdz/DZCyAcjH8Gg6Yxk1UK9SpTW/XVpzRupI8X2sa08szHpytebuMxV5SZO3vCZzcak3rGyu2gTHaEunx8S6tOeDQ2kffypvZnEgHja9qftm9zHlAhIvQdEAbmAEm/nhGFEeonnchMpGpcAxrnCMaJHNP0LcHKAWWb5IwEUg4rSchtCXaazI7fMOkbofCdUOPFB+M3tT+VCJi8ufWH9jHSPWt0n4asI2sUNBIGojzqRFIkAq+IqBQZ0avlcnEPEa6uHaSWljyr+jJfej0niHrZpcgNTos2Kzpkt3BYVN2fufPcvYkbW35Q/hnasGbc9B09d42t+/kXXOosNt1LpTcVkgKRr3+t7XxfSBta3LBGb6ajN2FtcIoS5hYppGftOmcO8SUi+0hTJ0cBelaDfyGmL9JmN5y/P2179905q5aNyz+Xe/zl9cf6JtWdujthc+fXEzZ33JtY23nvlRrTlp5kaWTyFpOop5A2mVrXeW96/cl705A6aS+bYTK9vjPoadZuWl2HpV0rP93S00OdyjvuTd3iMqVW6U6z44TN3REkzG3abTMxjc25yHY05SFfa9hqu+7+ufooR+KP5wR82zI/N/mTyXITzsu+KbLZsNSYbDJZFjGC9Ts5AVwS8vHg901+ZMLXlN80YdMDGxjB6hejY78xBsYYGGNgjIH/Ywb+DbS8aASxw9FIAAAAAElFTkSuQmCC";
 		public static final String FILE_1 = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAPgUlEQVR4XuWaW4xmx1W2n1X7+74+zYzHM/bYZowJzsjBcWY8B8+h52A7vooEImgGIRIHwQVIKBKWcscFuUAgcRtAwAURigQkQtANEpGChGR8nrEdx+P2iYzj+Ld/Oz7EnrHn1P31t3e99Hxd2ktV+obu6bFAFkvaXV2nXd96ax2rtknif5Ie+MuneOcnH4XT712obt6yodepwvg3//DeD3Di+VfeDD973WRsIh8bbd68iVF0WQAe/OFZ7xBgfCwkAYimEZ1OOAGclPh3M2aREIYQJhBOqQ8QAhBeej+GaHETSHDg5ksAXMso6nB54uYtExkG+ILeXrT5j/E+KPshRlE3zYHFQTywsBB/V+KYYBbAZAjhvAuVi5e7Y2ACjej33V81AE4XB/JlBNjlBQKBLOvzseUkg1BVdGXUVUPd2AxwDDSrTFpAmO89ZGAIA3mPDHD0EU5mloEKEFaQ1Xz3rewuNiRnvpyC5KWZEQyqYPR6FZEGYAZxtB1rPlEkhkVbCgMKaZPa8REDcXkaDYCjrpIJuSSY+f9SoRJZPT3mg828w4JRdSrGexWM9wBmBEcz8A3KH0MLhPdF1K4lOeJrAgCp0Pm8lEa0uRp4vZggM+R6TUyAVCFQxYYwOQYwAxxtxVggEoNSKx1CkAFULrxWAEpmCh2WlQp+uUXlY7AWGUWQJcOFQZIoRdEJUCUQJI5K1i5hyvbHgRRIahtVjFs7AFagkQoTOPJyUJR22OUeB893TWleFESJGEXTRIiRYEY3QGdyWR0MHVUC8h8e+jAkN1nohy+Q1ihN5hq8QIGmcFEwjJdefpU6RqpQYQ6AE0M3x53bb6OuI6dOvcYo2rbtUwwGNa+/8RYbN25gYmqcBqPbMVg/xuDcwgyyYxKzv3bPxvivz30UfmnHhjhqlyXfGYmrA0Cjpprv6u23f5pRVOqGBFUV0nhlniAqEhsRI9x0443UdUO3AmugFnSDYevH6Z9dmBE6Bjb7i0vM/+Of/ks49sAXI4B7FRUiL7gaL4DKavlyUPF4zUa/SpbNTiAgoImiHjRDprsVdEPaoWCMbxhPNkFHEfzqA78S/+kbzwVfDiSPMoUQoLV7gdK1CROYCoZV+npDMu8vvIWsHYXS+5BldiAYCYRlW2BAFYypjROZd7iw4YxJ0D6ojFPcWqxJBVpUBRiyvHPu+VOUtGPHbQDMzZ1iLTQ53qOyzTRA1wAMQ9QRMGPdtROcPzM/VIff/K17Z7/9nR/2vvSlzywiMpWTJeYLwzQ5+cssLj5PXb+2cjL0by+c0c/dtI4LA2EGEu7NcGmwwhtg5lvu81Ab0EJEGBCbyODS069ZmK/p9xf57LbrqBtRCwDqCHXUsGzShpw9fRGJY8DsX/zdi92v3n/HwCNGXBUMTPDFPdcBcOut8MEHX+Ps2W+sGApnVjRz5cJjfgNH3V2efKwDhuUWIgosYGbDhwCNIi7+lmxAUokAlYGZcc3mqVYdvnr/Zwd/9r2zQZ4Fgo1O3GIEaXClNsBAZMgCHo8XE+R9hTHOG2QBEjCk2CHIChtgyQawXK+gk5jbeN1UGzb/3hfWR2dYnjqXtpwG0JVlg2WwZ6k8efKlodWuOhVXQgZEibqu2XvXdiTxyo9e4+KFBSbHx6hsS2EDoI6CBIwhLMIgLoNw+qfnZzAdQzb7R9/+2/D1L38lCjKXu+ZASBJZZiahJPe7dn62bTdSX1uBvLPcCw+qLMBtt/088/M1cXGwzGRUigPAMMwSCCQQDEjGcdP165ZAODcjcezr939l9vf//pvhT778O1ESwNrjgAiAkUuui1UUnqJCYl6gYmwLpgGW+esoMAKSEXAx74ZRNsCSDRgC07rJwCUQ1iebwK9fYv6VN09WaXVkWhMATuamUFKWfhrmDBqZ3kl5fpCGlW7WsaqMyfEuwdpAaIQNsGQDcBAqCAabtqxD4jtIR7dtvbNxm2VrVIGYmJJBKv2UwHj66TlWS7t330FVVSvO2bntRirbQMOQsbSeFTaAZANEJ6kICBtKwhRLh60ziGMyzborWgMAEXyHJAzLDknu2rujzISx4kzMbYaKObkaNHXDYr9mvq55aO5tzpw5x6AWdROJTcTMqKpACIFOFRgb6zAxOcbkxBhTUz3GlkqrwAhs3DzJmfcvDhMo0OzaJcAJ4UyYDMwDDloGLU+hi5RVJlcHASaIaa6BBWPQiMXGCN0xLNYERWQRNQwBgUifyIX5Gj6cB4MmRszg+s3r2XLDRiamJqg3jnH2w/4M2DHE7NIJN/d9ZsOVASAJZdtriWGBDCE8RDSszMJw5pADZKIkLAU4VgWqXoduFCJQ1c2QwVgJtVKo7CyiipEmNrz77ke89dZpxid63HzL9UytG+PC+XpGxp4Xn3n9ufs+s72p5wNmqwagCGdTRQmVRx95Cic4ML2Lbre31P4k/x0duXs/TdPwxOPf5+OiTRu3UIUO1WDAwmLNK6feYnyiy9at11PX1R//wp4bfuO7z7w9P37DTRd/+nJY3cXIPz/zvm695RrmB8K9oEAOofC55tuycpkotgISaZrIoI4sLgzo92sW+gNirUtgDR8BJvPQPABqjWDKKGuaWkOpuTi/wPqpcTZvvu7Vqh7bS/9M/8//euPFp2a+xrnBt5C0khcAZUwakos8AGqDktQvMAdDngUgLGM/1RBCMoxAIBKqik5HjAliNxKbaghOjEKSv9XMQTfBEICqjb8mpybodrvI7NP9D9/Wj+P6ePvWE+Gdbe/GF1+2FVXAgxxnErcFwnBjSMGaipMaYfg7DZm/iyjvMyMEI1SBECOmCrOYwm2BGPanGcsgeKZG0zTJnoAwqqpDf6HmhQ9+Up/vTqpz7QY6X9gTqhu2xVXZAIEzL0Oe9nLi+DMMBgM63Q4oN2h1EzHg0JG9zF9c4OmnTrJWuuNzd9BE8Z8vvXz5+GHnDiLQS+xYSMmV2dL6Nc1HoXl7bFGbOpP2M5+6U2femVhZAjyrk0eEcuu+f3o3BRW6DgLGJyY4cs90EVmag6qkBhFijNR1gyKEyojNcr1rgZ27dgIiBMMl0VeqLIAp5Q5GVByWQpw5N9Cm86ZaFTf1r2Fq8+ZVJkMIr4ILn/nfQp/xPiRKYBxZwFAWFxi0ZwMCsECvZwBEmc8Lltb02MIM1EQwaBQxkvWJEJCZumZqsIU+22+bWkUyJCCL/sozgfwAxKFSOy/LGDIwAHes3j5kxH11UnoIhg2fQKgMc1yX+0yJmwAEglWEEJIqaNghA7OGoIo3+u+tLhKMgHyn8bjGOPG4xwH7Du6hssDxx5/m46Cde3YSDJ79wXOshnbdtRMzIRkhgJKnwkCkOnXoqFIc6zUW65UBUPStlYsxshT4HN6bxf2SOHBon6fE5vf8Zpn7c3fp19vFNdey29u1dxeVAQYxetit6O9UzI/rJTCM2KiVAMOqdrQqG+/3AxBXdINq+bb8bFAgT4YdJOT8y4hplKfG8uNRgUyQwJCcqZAMmjzmoKrcjVIFVzVz20MUUYBElKgiy8kU1sNYrImRhUaDix82qwuFETEmps0w1LYDI2J7w329A5k4bBOp1O4SgAhmRBnBRJTa7wdEMnBpvgVzGwUpfFJCDoIAgRq1TqshNrEhRkU6FVh3lbmAhItwcT54/NGnkMRqyfX1TiYmxnn4wcfo9XoJMIYxxZH7DjFYHPD9J5/lSmjfob0eFUrILOFhRLEMUSRYqBpVC3SDbCUAsqMvDzn9bn768D5ktCKO5elv6fOtkJK77zucgLU0TAjo9npMH9mPmRFjDruZyjglBZMGFrEIBE/Tlaa35iyKvoxw7c1XEAhlip/7XqDVc5Snv7nrSEC2wJTXZiIntwkQnNPyggJDkjvUNNRIeUNl6cygamR1E+u++gOjd+MtWsW9gFBbmh9qeL/re5Yn5NfTKlIfEFImDWme5V+j+OOeorylNWFJCpXWMFIsYYEQAmBYx5rAYtPp9WNTfxSn3j+3sgTQio1BJUT+A088+iRXQzt2b2dyapLjj5wYJjGhCiseYS+NG2Z4u/fvptfr8uiDjw2ZPHTvwTY+wdSqrAUwoH9hfmC9ismuNeu3Vrz62vxqjKBaEXfj4q7vwJEDgPDuQkoh9xRWpMsCDd8zjZEmI9c0PN+X2xI3LYIjnz/s80jrx7SACQhI0B+sbyZ77xDP9nj7R6f1wlwT4a6VVCD/mClB7GKp9BhIhqu8qwGpzG5X23l+aoCBxx1FqGuJOQnyq3b/TQLJ8DmpTgALVHFSZ6uu/v//u0YvfPe9+N7J11dzL5BeHMl1U77NwkC50XRvJP8WCOX95UcWcosthORcRgA5ww5ummeeosU0SDL8BCtSnT7Fc2yPz765N54/dz3j61itF5CjLdqdlMGJh59Y1t0QKCnG5UOM6SMHmb94kbkfzA39vI/1cYc/fwQQj//HYwB+W7xKEuLgPYeKr0lzaf3W39wfAfgDgN++slNhSS7K8ujtwN3TAKM0Htw7MD45wb7D+8moiAgBDi3ps78iH2uWxgnX+SzXVNqYMpLlqj6R8U9N5boWszNAc1Vx7CnzZGEI11NZPtYrVjLR5gRidMwgv4xBI14prfl2OF9OHgy6qpuKI3PlSRPlR5VCuEGUCiESTuabLS+hvKjxCBWzgnEZYiRdWSQonGJi7vgjj6GoK9LX0sUevPcwGDzh+t/2fW7XDmLT8NLci23f5W3AYUhznUQ0rf1uUMW9nsgj0um7DwPKJSOVyrJoc1eXZY9en14CwhhtAqYv2QYlACQYfcqW1sg9lgkkWyMAvlPIV2yZEEqMe1xurevDAXMXmDFZkkb/j6NaqJaywQ562Y+u4kPJURLkCLvfxsuck7Ka/DpXaK1l5bcHsAKIIZiPXbME+KKU5uTEw49nOrfn4D663d5S+2OMJhf14w/5mKsjf6+UJ4pOa7AB5cT+Qs34WMdbDPbfcwjAdR+Q/xiw4grAs2QOLI1BgLna4IkM7vJx1Rt91YhB3gc0jYjRA6E1q4BwGtQRLF/IVFyDleLvkpTmlp5OXje51/GxWS5ijv9odTSo6+iSuQoEwmqMIDiy3mdElBuizLe7WDiaXsVAbeltpf2R1x3s8mDFlp/YyF2p83EVKmBeixIhQgMoqgQHjQ6MR8c47teL9svXbYW1sNFGVWuPA1xeYxREoWCYDEUN2z4JZFyNFxBUnUBTxwyITwJVnYC4ikAIicmeLT0dPnm0RjdoZjcCPWDrX33vdebm3uETTc7XNPAWsCgpY8okjZpwC3ArHz8FIP4vzP2xpDdwygH4v0z/BSjKoeHVWVh6AAAAAElFTkSuQmCC";
--- FileChooser/src/com/ihhira/android/filechooser/MainActivity.java
+++ FileChooser/src/com/ihhira/android/filechooser/MainActivity.java
@@ -14,11 +14,20 @@ import android.widget.TextView;
 import com.ihhira.android.filechooser.FileChooser.DialogType;
 import com.ihhira.android.filechooser.FileChooser.FileSelectionCallback;
 
+/**
+ * 
+ * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+ */
 public class MainActivity extends Activity implements FileSelectionCallback {
 
 	protected static final String TAG = "FileChooser";
 	TextView tvStatus;
 
+	/* 
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 *(non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -60,6 +69,11 @@ public class MainActivity extends Activity implements FileSelectionCallback {
 
 	}
 
+	/* 
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 *(non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -67,11 +81,16 @@ public class MainActivity extends Activity implements FileSelectionCallback {
 		return true;
 	}
 
+	/* 
+	 * @author MD IMRAN HASAN HIRA ( imranhasanhira@gmail.com )
+	 *(non-Javadoc)
+	 * @see com.ihhira.android.filechooser.FileChooser.FileSelectionCallback#onSelect(java.io.File)
+	 */
 	@Override
 	public void onSelect(File file) {
 		String text = ">> " + file.getAbsolutePath();
 		Log.d(TAG, text);
-		tvStatus.append(text+"\n");
+		tvStatus.append(text + "\n");
 	}
 
 }
